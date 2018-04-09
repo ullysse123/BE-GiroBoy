d045f47d5c57ca5ad906416958a9e7d6cf0c0ad6
Julien <jujudu31140@hotmail.fr>
2018-04-09 16:48:28 +0100

    Gyro And Color sensor
    Adding functions to the Gyro ad IR Module

--- src/modules/GyroSensor.java
+++ src/modules/GyroSensor.java
@@ -1,5 +1,32 @@
 package modules;
 
-public class GyroSensor {
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.port.SensorPort;
+import lejos.robotics.SampleProvider;
 
+public class GyroSensor {
+	
+	EV3GyroSensor sensor;
+	SampleProvider sample;
+	float [] res = new float [sample.sampleSize()];
+	
+	public GyroSensor(){
+		sensor = new EV3GyroSensor(SensorPort.S2);
+		sensor.reset();
+	}
+	
+	float getAngle(){
+		SampleProvider sample= sensor.getAngleMode();
+		float [] res = new float [sample.sampleSize()];
+		sample.fetchSample(res, 0);
+		return res[0];
+	}
+	
+	float getAngularSpeed(){
+		SampleProvider sample= sensor.getRateMode();
+		float [] res = new float [sample.sampleSize()];
+		sample.fetchSample(res,0);
+		return res[0];
+	}
+	
 }
--- src/modules/InfraSensor.java
+++ src/modules/InfraSensor.java
@@ -1,5 +1,23 @@
 package modules;
 
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.port.SensorPort;
+import lejos.robotics.SampleProvider;
+
 public class InfraSensor {
 
+	EV3ColorSensor sensor;
+	
+	public InfraSensor(){
+		sensor = new EV3ColorSensor(SensorPort.S1);
+		
+	}
+	
+	float getColor(){
+		SampleProvider sample = sensor.getColorIDMode();
+		float [] res = new float [sample.sampleSize()];
+		sample.fetchSample(res, 0);
+		return res[0];
+	}
+	
 }
