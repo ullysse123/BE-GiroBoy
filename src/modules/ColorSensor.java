a55c8cfe802e6f8a3bd41df78b04e976ba42293f
Julien <jujudu31140@hotmail.fr>
2018-04-12 12:23:14 +0100

    Maj Color Sensor

---
+++ src/modules/ColorSensor.java
@@ -0,0 +1,27 @@
+package modules;
+
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.port.SensorPort;
+import lejos.robotics.SampleProvider;
+
+public class ColorSensor {
+
+	EV3ColorSensor sensor;
+	
+	public ColorSensor(){
+		sensor = new EV3ColorSensor(SensorPort.S1);
+		
+	}
+	
+	public float getColor(){
+		SampleProvider sample = sensor.getColorIDMode();
+		float [] res = new float [sample.sampleSize()];
+		sample.fetchSample(res, 0);
+		return res[0];
+	}
+	
+	public void close() {
+		sensor.close();
+	}
+	
+}
