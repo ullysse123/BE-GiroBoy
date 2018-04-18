8a247d6631d84e34646859bf5324fd7d4e5865b7
Syders <stevenalpha31@gmail.com>
2018-04-18 11:10:33 +0200

    MAJ AEtoile et Sommet

--- src/ia/AEtoile.java
+++ src/ia/AEtoile.java
@@ -9,9 +9,11 @@ public class AEtoile {
 	private List <Sommet> insererLesFils (List <Sommet> listAttentePrec, List <Sommet> listFils) {
 		List <Sommet> listAttente= listAttentePrec;
 		Iterator<Sommet>filsIterator=listFils.iterator();
+		//Parcours des fils
 		while(filsIterator.hasNext()) {
 			Sommet filsATraiter=filsIterator.next();
 			Boolean find=false;
+			//Parcours de la liste d'attente à trier
 			for(int i=0;i<listAttente.size() && !find; i++) {
 				Sommet attenteATraiter=listAttente.get(i);
 				if(find = filsATraiter.getGH()<attenteATraiter.getGH()) {
@@ -22,13 +24,36 @@ public class AEtoile {
 		return listAttente;
 	}
 	
-	//g dans la creation
-	public int[] fonction (Sommet sommetDepart) {
-		int [] directionList= {};
+	private Sommet creationFils (Sommet sommetPere,int cout) {
+		Sommet pere=sommetPere;
+		List <Sommet> sommetFils=new ArrayList<Sommet>();
+		for(Sommet fils:pere.getFilsList()) {
+			fils.setGh(fils.getGH());//heuristique a rajouter
+			fils.getListDirection().addAll(pere.getListDirection());
+			fils.getListDirection().add(fils.getDirection());
+			sommetFils.add(fils);
+		}
+		pere.setFilsList(sommetFils);
+		return pere;
+	}
+	
+	//Fonction aetoile
+	public List<Integer> fonction (Sommet sommetDepart,int cout,String but) {
+		List <Integer> directionList=new ArrayList<Integer>();
+		Boolean estBut=false;
 		List <Sommet> listAttente=new ArrayList <Sommet>();
-		listAttente=insererLesFils(listAttente , sommetDepart.getFilsList());
-		while(!listAttente.isEmpty()) {
-			
+		Sommet pereActuelle=creationFils(sommetDepart,cout);
+		listAttente=insererLesFils(listAttente , pereActuelle.getFilsList());
+		//Parcours de la liste d'attente
+		while(!listAttente.isEmpty() && !estBut) {
+			estBut=pereActuelle.getNom().equals(but);
+			if(estBut) {
+				directionList=pereActuelle.getListDirection();
+			}else {
+				pereActuelle=creationFils(listAttente.get(0),cout);
+				listAttente=insererLesFils(listAttente , pereActuelle.getFilsList());
+				listAttente.remove(0);
+			}
 		}
 		return directionList;
 	}
--- src/ia/Sommet.java
+++ src/ia/Sommet.java
@@ -1,32 +1,56 @@
 package ia;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sommet {
 
 	private String nom;
 	private int gh;
-	private List <Sommet> filsList;
-	private int [] direction;
+	private List <Sommet> filsList= new ArrayList <Sommet>();
+	private List <Integer> listDirection;
+	private int direction;
 	
-	public Sommet(String nom, int gh, List <Sommet> filsList, int [] direction) {
+	public Sommet(String nom, int gh, int direction) {
 		this.nom = nom;
 		this.gh = gh;
-		this.filsList = filsList;
 		this.direction=direction;
 	}
 	
 	
 
-	public void setDirection(int[] direction) {
-		this.direction = direction;
+
+	public void setListDirection(List<Integer> listDirection) {
+		this.listDirection = listDirection;
 	}
 
 
 
-	public int [] getDirection() {
+
+	public void setGh(int gh) {
+		this.gh = gh;
+	}
+
+
+
+	public void setFilsList(List<Sommet> filsList) {
+		this.filsList = filsList;
+	}
+
+
+	public int getDirection() {
 		return direction;
 	}
+
+
+
+
+	public List<Integer> getListDirection() {
+		return listDirection;
+	}
+
+
+
 
 	public String getNom() {
 		return nom;
