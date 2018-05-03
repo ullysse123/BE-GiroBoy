2e9c962e4dd01addb26288e812c169b0851a7bab
Syders <stevenalpha31@gmail.com>
2018-05-03 09:39:17 +0200

    Remise test pour Graph1

--- src/ia/HeuristiqueBase2.java
+++ src/ia/HeuristiqueBase2.java
@@ -1,14 +1,14 @@
 package ia;
 
-public class HeuristiqueBase implements Heuristique {
+public class HeuristiqueBase2 implements Heuristique {
 
 	@Override
 	public int fonction(Sommet s) {
 		int gh=s.getGH();
 		int nom=s.getNom();
 		int h=0;
-		if(nom!=6)h++;
-		if((nom>=3 && nom<6) || nom==1)h++;
+		if(nom!=1)h++;
+		if((nom>=2 && nom<5) || nom==6)h++;
 		return gh+h;
 	}
 
